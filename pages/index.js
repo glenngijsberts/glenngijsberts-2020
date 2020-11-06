f8778ac7efea777feee31c5a3457f1b7981e12ae
Glenn Gijsberts <glenngijsberts95@gmail.com>
2020-11-06 22:12:19 +0100

    Add Next SEO and comets icon library

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <NextSeo
+        title="Glenn Gijsberts | Front-end developer"
+        openGraph={{
+          title: 'Glenn Gijsberts | Front-end developer',
+          site_name: 'Glenn Gijsberts Portfolio',
+        }}
+      />
+
+      <h1>Hello world</h1>
+    </>
+  )
 }
