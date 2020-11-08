802eb4ae384a0d32d3e817acffe1eae51b8be463
Glenn Gijsberts <glenngijsberts95@gmail.com>
2020-11-08 22:12:37 +0100

    Edit global styles
    Replaced Poppins with Inter font. Added custom cursor. Added primary color variable.

--- components/GlobalStyles/index.js
+++ components/GlobalStyles/index.js
@@ -1,46 +1,47 @@
 import React from 'react'
 import { Global, css } from '@emotion/core'
-import { globalStyles as globalStylesSolar } from '@ticketswap/solar'
+import { globalStyles as globalStylesSolar, color } from '@ticketswap/solar'
+import { themeColor } from '~/theme'
 
 const globalStyles = css`
   @font-face {
-    font-family: 'Poppins';
-    src: url('/fonts/Poppins-Regular.ttf');
+    font-family: 'Inter';
+    src: url('/fonts/Inter-Regular.ttf');
     font-style: normal;
     font-weight: 300;
     font-display: auto;
   }
   @font-face {
-    font-family: 'Poppins';
-    src: url('/fonts/Poppins-Medium.ttf');
+    font-family: 'Inter';
+    src: url('/fonts/Inter-Medium.ttf');
     font-style: normal;
     font-weight: 400;
     font-display: auto;
   }
   @font-face {
-    font-family: 'Poppins';
-    src: url('/fonts/Poppins-SemiBold.ttf');
+    font-family: 'Inter';
+    src: url('/fonts/Inter-SemiBold.ttf');
     font-style: normal;
     font-weight: 500;
     font-display: auto;
   }
   @font-face {
-    font-family: 'Poppins';
-    src: url('/fonts/Poppins-Bold.ttf');
+    font-family: 'Inter';
+    src: url('/fonts/Inter-Bold.ttf');
     font-style: normal;
     font-weight: 600;
     font-display: auto;
   }
   @font-face {
-    font-family: 'Poppins';
-    src: url('/fonts/Poppins-ExtraBold.ttf');
+    font-family: 'Inter';
+    src: url('/fonts/Inter-ExtraBold.ttf');
     font-style: normal;
     font-weight: 700;
     font-display: auto;
   }
   @font-face {
-    font-family: 'Poppins';
-    src: url('/fonts/Poppins-Black.ttf');
+    font-family: 'Inter';
+    src: url('/fonts/Inter-Black.ttf');
     font-style: normal;
     font-weight: 800;
     font-display: auto;
@@ -90,13 +91,20 @@ const globalStyles = css`
   }
 
   *::selection {
-    background-color: orange;
+    background-color: ${themeColor.primary};
     color: white;
   }
 
+  img::selection {
+    background-color: transparent;
+  }
+
   body {
-    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'avenir next', avenir,
-      'helvetica neue', helvetica, ubuntu, roboto, noto, 'segoe ui', arial, sans-serif !important;
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'avenir next', avenir, 'helvetica neue',
+      helvetica, ubuntu, roboto, noto, 'segoe ui', arial, sans-serif !important;
+    min-height: 100vh;
+    color: #292926;
+    cursor: url(/cursor.png), auto !important;
   }
 `
 
